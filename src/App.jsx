a99418d7b8f2731df47a5057cbf8d20890bbf0fa
Hatice Kaplan <4haticekaplan4@gmail.com>
2024-09-23 09:33:12 +0200

    Implement winner calculation and status display
    - Added calculateWinner function to determine the game's winner.
    - Updated status message to show the winner or indicate the next player.
    - Prevented further moves once a winner is declared.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 
-import { useState } from 'react';
-import './App.css'
-function Square({value,onSquareClick}) {
+import React, { useState } from 'react';
+import './App.css';
 
+function Square({value,onSquareClick}) {
   return (
   <button className='square' onClick={onSquareClick}>{value}</button>)
 }
 
 export default function Board() {
-  const [xIsNext,setXIsNext] = useState(true);
+const [xIsNext,setXIsNext] = useState(true);
 const [squares,setSquares] = useState(Array(9).fill(null));
 function handleClick(i){
-  if(squares[i]){
+  if(calculateWinner(squares) || squares[i]){
     return;
   }
   const nextSquares = squares.slice();
@@ -23,8 +23,16 @@ function handleClick(i){
   setSquares(nextSquares);
   setXIsNext(!xIsNext);
 }
+const winner = calculateWinner(squares);
+let status;
+if(winner){
+  status = "Winner :" + winner;
+}else{
+  status = "Next Player: " + (xIsNext ? "X" : "O");
+}
   return (
     <>
+    <div className='status'>{status}</div>
       <div className='row-square'>
         <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
@@ -43,4 +51,23 @@ function handleClick(i){
 
     </>
   )
+}
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for(let i=0;i < lines.length;i++){
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
